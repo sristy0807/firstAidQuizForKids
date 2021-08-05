ca5575040cad2539fadaecc00ae159fb80c7c4ef
Sheikh Saad Sristy <sristy.cse@gmail.com>
2021-08-06 00:45:35 +0600

    UI set up for several scenes. Basic panel navigation functionalities added

---
+++ FirstAidAndroid/Assets/Scripts/PanelChanger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelChanger : MonoBehaviour
+{
+    public GameObject[] UIPanels;
+
+    public static int currentActivePanelID;
+
+    public void GoToPanell(int id)
+    {
+        if (id < UIPanels.Length)
+        {
+            UIPanels[id].gameObject.SetActive(true);
+            UIPanels[currentActivePanelID].gameObject.SetActive(false);
+            currentActivePanelID = id;
+        }
+        else
+        {
+            Debug.Log("No where to go");
+        }
+            
+      
+    }
+
+
+}
